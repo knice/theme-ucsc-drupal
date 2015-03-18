e04c33891d8ef10e100abdb1c61cd4c680e85d31
kanarb <kanarb@ucsc.edu>
2015-03-18 16:17:32 -0700

    Changed divs to classes and dashes to underscores in page-home-left.tpl.php

--- partials/page-home-left.tpl.php
+++ partials/page-home-left.tpl.php
@@ -6,15 +6,15 @@
 		<div class="grid_12">
 	<?php } ?>
 	
-		<h1 id="page-title"><?php print $title; ?></h1>
+		<h1 class="page_title"><?php print $title; ?></h1>
 
 		<?php if($page["above_content"]): ?>
-		  <div id="above-content">
+		  <div class="above_content">
 		    <?php print render($page["above_content"]); ?>
 		  </div>
 		<?php endif; ?>
 
-		<div id="content">
+		<div class="content">
 			<?php if ($show_messages && $messages): print render($messages); endif; ?>
 			<?php if($page["help"]): print '<div class="help">' . render($page["help"]) . '</div>'; endif; ?>
 			<?php print render($tabs); ?>
@@ -23,7 +23,7 @@
 			<div class="clear"></div>
 		</div>
 		<?php if($page["below_content"]): ?>
-		    <div id="below-content">
+		    <div class="below_content">
 		        <?php print render($page["below_content"]); ?>
 		        <div class="clear"></div>
 		    </div>
@@ -31,7 +31,7 @@
 	</div>
 
 	<?php if ($page["sidebar_first"]) { ?>
-	<div class="grid_3 pull_9" id="left-content">
+	<div class="grid_3 pull_9 left_content">
 		<?php print render($page["sidebar_first"]); ?>
 	</div>
 	<?php } ?>
