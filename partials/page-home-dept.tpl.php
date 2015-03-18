b845ca892456c3655b548a13731c20b03cc32b9a
kanarb <kanarb@ucsc.edu>
2015-03-18 16:12:18 -0700

    Changed divs to classes in page.home.dept.tpl.php

--- partials/page-home-dept.tpl.php
+++ partials/page-home-dept.tpl.php
@@ -2,34 +2,34 @@
 <!-- Default homepage template -->
 
 <?php if ($page["above_content"]): ?>
-	<div id="above_content" class="grid_12">
+	<div class="grid_12 above_content">
 		<?php print render($page["above_content"]); ?>
 	</div>
 	<div class="clear"></div>
 <?php endif; ?>
 
 <div class="grid_12">
-	<div id="three_column">
-		<div class="grid_4 alpha" id="three_column_one">
+	<div class="three_column">
+		<div class="grid_4 alpha three_column_one">
 			<?php print render($page["three_column_one"]); ?>
 		</div>
-		<div class="grid_4" id="three_column_two">
+		<div class="grid_4 three_column_two">
 			<?php print render($page["three_column_two"]); ?>
 		</div>
-		<div class="grid_4 omega" id="three_column_three">
+		<div class="grid_4 omega three_column_three">
 			<?php print render($page["three_column_three"]); ?>
 		</div>
 	</div>
 	<div class="clear"></div>
 
-	<div id="dept_three_column" class="three combined light">
-		<div class="grid_4 alpha" id="dept_column_one">
+	<div class="three combined light">
+		<div class="grid_4 alpha">
 			<?php print render($page["dept_column_one"]); ?>
 		</div>
-		<div class="grid_4" id="dept_column_two">
+		<div class="grid_4">
 			<?php print render($page["dept_column_two"]); ?>
 		</div>
-		<div class="grid_4 omega" id="dept_column_three">
+		<div class="grid_4 omega">
 			<?php print render($page["dept_column_three"]); ?>
 		</div>
 		<div class="clear"></div>
@@ -38,7 +38,7 @@
 
 <?php if($page["below_content"]): ?>
 	<div class="clear"></div>
-	<div id="below_content" class="grid_12">
+	<div class="grid_12 below_content">
 		<?php print render($page["below_content"]); ?>
 	</div>
 <?php endif; ?>
