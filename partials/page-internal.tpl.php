9a795e24a57607efad27992b9d6a14ba48a8077d
kanarb <kanarb@ucsc.edu>
2015-03-18 16:23:15 -0700

    Changed divs to classes and dashes to underscores on page-internal.tpl.php

--- partials/page-internal.tpl.php
+++ partials/page-internal.tpl.php
@@ -2,7 +2,7 @@
 <!-- Internal page template -->
 
 	<?php if ($breadcrumb != ""): ?>
-	<div id="breadcrumbs" class="grid_12">
+	<div class="breadcrumbs" class="grid_12">
 	<?php print render($breadcrumb); ?>
 	</div>
 	<?php endif; ?>
@@ -14,16 +14,16 @@
 		<div class="grid_12">
 	<?php } ?>
 
-		<h1 id="page-title"><?php print $title; ?></h1>
+		<h1 class="page_title"><?php print $title; ?></h1>
 	
 	<?php if($page["above_content"]): ?>
-	  <div id="above-content" class="grid_12">
+	  <div class="above_content" class="grid_12">
 	    <?php print render($page["above_content"]); ?>
 	  </div>
 	<?php endif; ?>
 
 
-	<div id="content">
+	<div class="content">
 		<?php if ($show_messages && $messages): print render($messages); endif; ?>
 		<?php if($page["help"]): print '<div class="help">' . render($page["help"]) . '</div>'; endif; ?>
 		<?php print render($tabs); ?>
@@ -34,7 +34,7 @@
 
 
 	<?php if($page["below_content"]): ?>
-	    <div id="below-content">
+	    <div class="below_content">
 	        <?php print render($page["below_content"]); ?>
 	        <div class="clear"></div>
 	    </div>
@@ -43,7 +43,7 @@
 	</div>
 
 	<?php if ($page["sidebar_first"]) { ?>
-	<div class="grid_3 pull_9" id="left-content">
+	<div class="grid_3 pull_9 left_content">
 		<?php print render($page["sidebar_first"]); ?>
 	</div>
 	<?php } ?>
