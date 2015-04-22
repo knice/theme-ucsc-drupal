d82a0937cb7c0d152cac9412e34660ab99e26388
kanarb <kanarb@ucsc.edu>
2015-04-22 15:54:39 -0700

    Fixes to search block

--- template.php
+++ template.php
@@ -5,4 +5,9 @@ function ucsc_form_system_site_information_settings_alter(&$form, &$form_state, 
     "To insert a line break into your site title, use two consecutive spaces.";
 }
 
+function ucsc_form_alter(&$form, &$form_state, $form_id) {
+  $sitename = "Search " . variable_get('site_name');
+  $form['search_block_form']['#attributes']['placeholder'] = t($sitename);
+}
+
 ?>
